32b6b31747f3531cbbf0b08ce369e47d861b09df
Hyeonjae-K <href.hjk@gmail.com>
2022-07-17 19:33:43 +0900

    Setting Up Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:flutter_instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:flutter_instagram_clone/responsive/web_screen_layout.dart';
 import 'package:flutter_instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyChxzbKB0f_bsRuXZL1P2dRI-TVbLLEM6E",
+        appId: "1:1032461596954:web:3422bbc082ff763ce9d4a3",
+        messagingSenderId: "1032461596954",
+        projectId: "instagram-clone-334b6",
+        storageBucket: "instagram-clone-334b6.appspot.com",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
@@ -19,7 +35,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: mobileBackgroundColor,
       ),
-      home: ResponsiveLayout(
+      home: const ResponsiveLayout(
         webScreenLayout: WebScreenLayout(),
         mobileScreenLayout: MobileScreenLayout(),
       ),
