03141d9a31e3d889f180e6ddc26b1fcc1f125f78
Hyeonjae-K <href.hjk@gmail.com>
2022-07-20 22:36:57 +0900

    User Data State Management

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,13 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_instagram_clone/models/user.dart' as model;
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
 
   @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  String username = "";
+
+  void getUsername() async {
+    DocumentSnapshot snap = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get();
+
+    setState(() {
+      username = (snap.data() as Map<String, dynamic>)['username'];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUsername();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    model.User user = Provider.of<UserProvider>(context).getUser;
+    return Scaffold(
       body: Center(
-        child: Text('this is mobile'),
+        child: Text(user.email),
       ),
     );
   }
